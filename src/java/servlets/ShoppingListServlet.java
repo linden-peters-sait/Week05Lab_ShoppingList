1b4302a165dcaa857d9fea5628e252255578ee11
Linden Peters <linden.peters@sait.ca>
2020-02-11 15:08:50 -0700

    Added register mechanism, session checking, and action handler.

--- src/java/servlets/ShoppingListServlet.java
+++ src/java/servlets/ShoppingListServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -30,7 +31,13 @@ public class ShoppingListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        String user = (String)session.getAttribute("username");
+        if (user != null && !user.equals("")) {
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        }
     }
 
     /**
@@ -44,7 +51,17 @@ public class ShoppingListServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        String action = request.getParameter("action");
+        if (action != null && !action.equals("")) {
+            if (action.equals("register")) {
+                String fldUsername = request.getParameter("fldUsername");
+                if (fldUsername != null && !fldUsername.equals("")) {
+                    session.setAttribute("username", fldUsername);
+                }
+            } // Add actions here.
+        }
+        response.sendRedirect("ShoppingList");
     }
 
     /**
